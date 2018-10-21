e06a4eaed8adc177b5d9a25a3be754f4563b000b
Cory House <housecor@gmail.com>
2018-10-21 17:08:00 -0500

    Remove pre m4 stuff

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 class Nav extends Component {
   render() {
-    const { isAuthenticated, login, logout } = this.props.auth;
     return (
       <nav>
         <ul>
