4b5f8dee7129f2f9ea86e25804f6a5eb1fdfb7ec
Cory House <housecor@gmail.com>
2018-10-29 18:32:43 -0500

    Remove destructure from m7

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 class Nav extends Component {
   render() {
-    const { isAuthenticated, login, logout, userHasScopes } = this.props.auth;
+    const { isAuthenticated, login, logout } = this.props.auth;
     return (
       <nav>
         <ul>
