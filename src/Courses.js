5814602c2667f7b5cb3c367e4648df1510c58a8a
Cory House <housecor@gmail.com>
2018-11-14 18:36:39 -0600

    Remove delete course feature

--- src/Courses.js
+++ src/Courses.js
@@ -17,37 +17,11 @@ class Courses extends Component {
       .catch(error => this.setState({ message: error.message }));
   }
 
-  deleteCourse(courseId) {
-    fetch(`/course/${courseId}`, {
-      method: "DELETE",
-      headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
-    })
-      .then(response => {
-        if (response.ok) return response.json();
-        throw new Error("Network response was not ok.");
-      })
-      .then(response =>
-        this.setState(state => ({
-          courses: state.courses.filter(course => course.id !== courseId)
-        }))
-      )
-      .catch(error => this.setState({ message: error.message }));
-  }
-
   render() {
     return (
       <ul>
         {this.state.courses.map(course => {
-          return (
-            <li key={course.id}>
-              {this.props.auth.userHasScopes(["delete:courses"]) && (
-                <button onClick={() => this.deleteCourse(course.id)}>
-                  Delete
-                </button>
-              )}{" "}
-              {course.title}
-            </li>
-          );
+          return <li key={course.id}>{course.title}</li>;
         })}
       </ul>
     );
